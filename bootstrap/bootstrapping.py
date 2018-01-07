5b01b4a0df2831e5227fcf48e56b5df4e6bb3380
Adam Szabunio <adamszabunio@MacBook-Pro.local>
2018-01-06 19:06:23 -0800

    bootstrap result analysis and organizing dirs

--- bootstrap/bootstrapping.py
+++ bootstrap/bootstrapping.py
@@ -7,25 +7,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import DistanceMetric, BallTree
 
 
-def k_neighbors(k, pop_list, full_data, neigh_table, neigh_dist, s=1): 
-    '''
-    Return two values:
-    mean of the k-nearest neighbors, and inverse weighted mean.
-    (e.g. crop yield measured in bushels/acre)
-    INPUTS: 
-    "k"= int, range(1-10), number of neighbors to use for computing missing values.
-    "pop_list"= random discrete uniform distribution array of years. used for indexing missing values.
-    "full_data"= complete data. used for looking up true values of "k" nearest points.
-    "neigh_table"= array of "k" nearest points for "full_data".
-    "neigh_dist"= distances array of "neigh_table".
-    "s"= IDW (Inverse Distance Weighting) exponent, default=1/(dist**1), where dist=distance to neighbor. 
-        (e.g. 1/(dist**"s"))
-    OUTPUTS: 
-    "kN_mean_preds"= list of "k" nearest neighbor predictions (continuous value for each pred). 
-    "kN_IDW_preds"= IDW average using hyperparameter "s" (continuous value for each pred).
-    '''
-    counties = full_data.shape[0]
-    kN_mean_preds, kN_IDW_preds = [], []
+def k_neighbors(k, pop_list, counties, s=1): 
+    kN_mean_preds, kN_weighted_preds = [], []
     for i in range(counties):
         neighbors = []
         for j in neigh_table.iloc[i][:k]:
@@ -33,47 +16,25 @@ def k_neighbors(k, pop_list, full_data, neigh_table, neigh_dist, s=1):
             neighbors.append(n)
         kN_mean_preds.append(np.mean(neighbors))
         w = list(map(lambda x: x**-s, neigh_dist.iloc[i][:k]))
-        kN_IDW_preds.append(np.average(neighbors, weights=w))
+        kN_weighted_preds.append(np.average(neighbors, weights=w))
         
     return kN_mean_preds, kN_weighted_preds
 
 def mape(y_pred, y_true):
-    '''Returns the Mean Absolute Precentage Error'''
     y_pred = np.asarray(y_pred)
     y_true = np.asarray(y_true)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-def bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV_LR, full_HAV_EXP2_LR):
-    '''
-    Return two dataframes: 
-    "predictions" of all missing values per techinque.
-    "metrics" of each technique (MAPE, MAE, RMSE, RSQUARED).
-    INPUTS: 
-    "full_data"= complete data. used for looking up true values.
-    "hav_dist_df"= pairwise dataframe of haversine distances.
-    "neigh_table"= array of "k" nearest points for "full_data".
-    "neigh_dist"= distances array of "neigh_table".
-    "full_LR"= dataframe of 'complete data' linear regression predicitons
-    "full_HAV_LR"= dataframe of 'complete data' Weighted linear regression predicitons \
-        using IDW (Inverse Distance Weighting) with an exponent of 1 sum(1/(dist**1)), \
-        where dist=distance to neighbors. 
-    "full_HAV_EXP2_LR"= same as above, with an IDW exponent of 2
-    OUTPUTS: 
-    "predictions"= dataframe of prediciton for all missing values per techinque.
-        all techniques explained in the README.
-    "metrics"= dataframe metric results of each technique (MAPE, MAE, RMSE, RSQUARED).
-    '''
-    # number of counties in the complete dataset
-    counties = full_data.shape[0]
+def bootstrap(counties):
     # create random test set
-    pop_list = np.random.randint(1970, 2017, counties)
+    pop_list = np.random.randint(1970,2017, full_data.shape[0])
     true = [full_data.iloc[i][yr] for i, yr in enumerate(pop_list)]
 
     full_test = full_data.copy() # make a copy before removing data
     for i, yr in enumerate(pop_list):
         full_test.iloc[i][yr] = np.nan 
 
-    # linear regression preds using only individual county data
+        # linear regression preds using only individual county data
     y_pred = []
 
     for i in range(counties):
@@ -90,20 +51,22 @@ def bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV
 
 
     ffill_df = full_test.copy()
-    # fill in missing value from the previous year
     ffill_df.fillna(method='ffill', axis=1, inplace=True)
     # unless it is the first year in the series, then fill in value from the second year
     ffill_df.fillna(method='bfill', axis=1, inplace=True)
-    ffill_preds = [ffill_df.iloc[i][pop_list[i]] for i in range(counties)]
+
+    ffill_preds = [ffill_df.iloc[i][pop_list[i]] for i in range(full_test.shape[0])]
 
     # fill in missing values (1) with the mean of all years for that specific county
     #yr_mean_df = full_test.copy()
     #yr_mean_df = yr_mean_df.apply(lambda x: x.fillna(x.mean()),axis=1)
+
     #yr_mean_preds = [yr_mean_df.iloc[i][pop_list[i]] for i in range(counties)]
 
-    # fill in missing values (random num per year) with the mean of associated year 
+    # fill in missing values (random number per year) with the mean of each year 
     county_mean_df = full_test.copy()
     county_mean_df = county_mean_df.apply(lambda x: x.fillna(x.mean()),axis=0)
+
     county_mean_preds = [county_mean_df.iloc[i][pop_list[i]] for i in range(counties)]
 
     #save baseline and individual county linear regression predicitions 
@@ -114,16 +77,16 @@ def bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV
     #predictions["yr_mean_pred"] = yr_mean_preds
     predictions["county_mean_pred"] = county_mean_preds
 
-    # inverse distance weighting (haversine distance)
+    # exponential smoothing (haversine distance)
     for i in [.25, .5, 1, 2, 4, 8, 16]:
-        hav_IDW = hav_dist_df.copy()
-        hav_IDW = hav_IDW.applymap(lambda x: x**-i if x != 0 else 0)
-        hav_IDW = hav_IDW.apply(lambda x: x/sum(x), axis=1)
+        hav_exp_smooth = hav_dist_df.copy()
+        hav_exp_smooth = hav_exp_smooth.applymap(lambda x: x**-i if x != 0 else 0)
+        hav_exp_smooth = hav_exp_smooth.apply(lambda x: x/sum(x), axis=1)
 
-        hav_IDW_vals = np.array(hav_IDW).dot(np.array(full_data))
+        hav_exp_smooth_vals = np.array(hav_exp_smooth).dot(np.array(full_data))
 
         predictions["exp{}_pred".format(i)] = \
-        [hav_IDW_vals[j][pop_list[j]-1970] for j in range(counties)]
+        [hav_exp_smooth_vals[j][pop_list[j]-1970] for j in range(counties)]
 
     # k Nearest Neighbor preds
     kN1_preds = []
@@ -131,20 +94,13 @@ def bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV
         n = full_data.iloc[neigh_table.iloc[i][0]][pop_list[i]]
         kN1_preds.append(n)
 
-    kN2_mean_preds, kN2_weighted_preds = \
-        k_neighbors(2, pop_list, full_data, neigh_table, neigh_dist)
-    kN3_mean_preds, kN3_weighted_preds = \
-        k_neighbors(3, pop_list, full_data, neigh_table, neigh_dist)
-    kN4_mean_preds, kN4_weighted_preds = \
-        k_neighbors(4, pop_list, full_data, neigh_table, neigh_dist)
-    kN5_mean_preds, kN5_weighted_preds = \
-        k_neighbors(5, pop_list, full_data, neigh_table, neigh_dist)
-    kN10_mean_preds, kN10_weighted_preds = \
-        k_neighbors(10, pop_list, full_data, neigh_table, neigh_dist)
-    _, kN5_exp2weighted_preds = \
-        k_neighbors(5, pop_list, full_data, neigh_table, neigh_dist, s=2)
-    _, kN10_exp2weighted_preds = \
-        k_neighbors(10, pop_list, full_data, neigh_table, neigh_dist, s=2)
+    kN2_mean_preds, kN2_weighted_preds = k_neighbors(2, pop_list, counties)
+    kN3_mean_preds, kN3_weighted_preds = k_neighbors(3, pop_list, counties)
+    kN4_mean_preds, kN4_weighted_preds = k_neighbors(4, pop_list, counties)
+    kN5_mean_preds, kN5_weighted_preds = k_neighbors(5, pop_list, counties)
+    kN10_mean_preds, kN10_weighted_preds = k_neighbors(10, pop_list, counties)
+    _, kN5_exp2weighted_preds = k_neighbors(5, pop_list, counties, s=2)
+    _, kN10_exp2weighted_preds = k_neighbors(10, pop_list, counties, s=2)
 
     predictions["k1_pred"] = kN1_preds
     predictions["k2_mean_pred"] = kN2_mean_preds
@@ -160,7 +116,7 @@ def bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV
     predictions["k5_exp2weighted_pred"] = kN5_exp2weighted_preds
     predictions["k10_exp2weighted_pred"] = kN10_exp2weighted_preds
     
-    # Look up the missing values in the complete data linear regression tables 
+    # Looking up the missing values in the FULL linear regression tables 
     FULL_LR_preds = [full_LR.iloc[i][pop_list[i]] for i in range(counties)]
     FULL_HAV_LR_preds = [full_HAV_LR.iloc[i][pop_list[i]] for i in range(counties)]
     FULL_HAV_LR_EXP2_preds = [full_HAV_EXP2_LR.iloc[i][pop_list[i]] for i in range(counties)]
@@ -186,12 +142,12 @@ def bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV
 
 
 # load full data 
-full_data = pd.read_csv("../data/ts_corn_47yr_df.csv")
+full_data = pd.read_csv("data/ts_corn_47yr_df.csv")
 full_data.fips_code = full_data.fips_code.astype(str).apply(lambda x: x.zfill(5))
 full_data.set_index('fips_code', inplace=True)
 full_data.columns = full_data.columns.astype(int)
 # load county centroids (inner join with the data)
-centroids_df = pd.read_csv("../data/county_centroids.csv", index_col=0)
+centroids_df = pd.read_csv("data/county_centroids.csv", index_col=0)
 centroids_df.fips_code = centroids_df.fips_code.astype(str).apply(lambda x: x.zfill(5))
 centroids_df.set_index('fips_code', inplace=True)
 full_data_centroids = full_data.join(centroids_df) 
@@ -203,36 +159,36 @@ hav_dist_df = pd.DataFrame(dist.pairwise(full_hav_df))
 
 # load predictions from FULL linear regression models 
 # will be used for indexing later
-full_LR = pd.read_csv("../data/lin_reg_preds.csv", index_col="fips_code")
+full_LR = pd.read_csv("data/lin_reg_preds.csv", index_col="fips_code")
 full_LR.columns = full_LR.columns.astype(int)
 
-full_HAV_LR = pd.read_csv("../data/lin_reg_haversine_norm_preds.csv", index_col="fips_code")
+full_HAV_LR = pd.read_csv("data/lin_reg_haversine_norm_preds.csv", index_col="fips_code")
 full_HAV_LR.columns = full_HAV_LR.columns.astype(int)
 
-full_HAV_EXP2_LR = pd.read_csv("../data/lin_reg_hav_norm_exp2_df.csv", index_col="fips_code")
+full_HAV_EXP2_LR = pd.read_csv("data/lin_reg_hav_norm_exp2_df.csv", index_col="fips_code")
 full_HAV_EXP2_LR.columns = full_HAV_EXP2_LR.columns.astype(int)
 
 # load nearest neighbors and distances
-neigh_table = pd.read_csv("../data/k10neighbors.csv", index_col=0)
-neigh_dist = pd.read_csv("../data/k10_distances.csv", index_col=0)
+neigh_table = pd.read_csv("data/k10neighbors.csv", index_col=0)
+neigh_dist = pd.read_csv("data/k10_distances.csv", index_col=0)
+
+counties = full_data.shape[0]
+
+print("Starting 1000 Bootstrap samples...")
+all_predictions, all_metrics = bootstrap(counties)
 
 iterations = 1000
-
-print("Starting {} Bootstrap samples...".format(iterations))
-all_predictions, all_metrics = bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV_LR, full_HAV_EXP2_LR)
 
 for bs in range(1, iterations):
     if (bs)%10 == 0:
         print("Starting Iteration {}".format(bs))
-    predictions, metrics = bootstrap(full_data, hav_dist_df, neigh_table, neigh_dist, full_LR, full_HAV_LR, full_HAV_EXP2_LR)
-    all_predictions = all_predictions.join(predictions, rsuffix=bs)  
+    predictions, metrics = bootstrap(counties)
     all_metrics = all_metrics.join(metrics, rsuffix=bs)
-    
-print("Bootstrapping complete.")
+    all_predictions = all_predictions.join(predictions, rsuffix=bs)  
+        
+all_metrics.to_csv("data/bootstrap_corn_metrics.csv")
+all_predictions.to_csv("data/bootstrap_corn_predicitions.csv")
 
-# save all predictions and metrics 
-all_predictions.to_csv("../data/bootstrap_corn_predicitions.csv")
-all_metrics.to_csv("../data/bootstrap_corn_metrics.csv")
 
 
 
